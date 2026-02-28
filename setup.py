bb8dbc534492828747665780fe34e88c71860976
Ryan Heard <ryanwheard@gmail.com>
2026-02-28 17:44:17 -0500

    fix mypyc warning

--- setup.py
+++ setup.py
@@ -10,9 +10,13 @@ setup(
     #
     # However: When I do that, quadint/__init__.py *itself* is included in the wheel which we don't want,
     #   because then the python version will be used instead of the mypyc-compiled pyd version.
-    packages=["quadint-stubs"],
+    packages=["quadint-stubs", "quadint-stubs.quad"],
     include_package_data=True,
     package_data={"quadint-stubs": ["*.pyi", "**/*.pyi"]},
-    ext_modules=mypycify(glob("quadint/**/*.py", recursive=True)),  # noqa: PTH207
+    ext_modules=mypycify(
+        glob("quadint/**/*.py", recursive=True),  # noqa: PTH207
+        strip_asserts=True,
+        strict_dunder_typing=True,
+    ),
     license="MIT",
 )
