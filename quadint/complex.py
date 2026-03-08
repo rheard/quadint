3f18562ecb75208cf2300d2bd55488570cc8872c
Ryan Heard <ryanwheard@gmail.com>
2026-03-08 00:22:53 -0500

    remove redundant method

--- quadint/complex.py
+++ quadint/complex.py
@@ -21,22 +21,6 @@ class complexint(QuadInt):
     SYMBOL: ClassVar[str] = "j"
     DEFAULT_RING = _ZI
 
-    def _from_obj(self, n: complex | int | float | QuadInt) -> complexint:
-        """Make a QuadInt on the current ring from a given object"""
-        if isinstance(n, (int, float)):
-            a = int(n)
-            b = 0
-        elif isinstance(n, complex):
-            a = int(n.real)
-            b = int(n.imag)
-        elif isinstance(n, complexint):
-            return n
-        else:
-            return NotImplemented
-
-        # The only time b is not 0 is if self.den is 1 anyway... No need to multiply
-        return self._make(a * self.ring.den, b)
-
     @property
     def real(self) -> int:
         """Alias for complexint"""
